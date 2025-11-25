f6d2345621a2c84eecdaaaf185c304d6bb797850
Bogdan <bogdan30-95@mail.ru>
2025-11-25 18:16:28 +0300

    add @Attachment addVideo()

--- src/test/java/chufaroff/projects/pages/TestBase.java
+++ src/test/java/chufaroff/projects/pages/TestBase.java
@@ -12,13 +12,20 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.attachment;
 
 @ExtendWith({BrowserPerTestStrategyExtension.class})
 public class TestBase {
-    @BeforeAll
 
+    @BeforeAll
     static void setUp() {
         // Конфигурация Selenide
         Configuration.browserSize = "1920x1080";
@@ -33,6 +40,13 @@ public class TestBase {
                 .savePageSource(true)
                 .includeSelenideSteps(true)
         );
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
@@ -47,7 +61,7 @@ public class TestBase {
     // Метод возвращает byte[] - байтовый массив скриншота
     @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
     public byte[] takeScreenshot() {
-        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
     @Attachment(value = "Page Source", type = "text/html", fileExtension = ".html")
@@ -55,16 +69,39 @@ public class TestBase {
         return WebDriverRunner.source();
     }
 
-    @Attachment(value = "Console Logs", type = "text/plain", fileExtension = ".txt")
-    public static String attachConsoleLogs() {
-        return String.join("\n", Selenide.getWebDriverLogs("browser"));
+    public static void attachmentPageSource() {
+        attachment("Page Source", WebDriverRunner.source());
     }
     // webdriver().driver().source() - получает полный HTML-код страницы, включая весь отрендеренный DOM
     // attachment("Source", ...) - создает вложение в Allure-отчет с названием "Source" и полученным HTML-содержимым
     // attachment("Source", webdriver().driver().source());
 
-    public static void attachmentPageSource() {
-        attachment("Page Source", WebDriverRunner.source());
+    @Attachment(value = "Console Logs", type = "text/plain", fileExtension = ".txt")
+    public static String attachConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs("browser"));
     }
+
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl(getSessionId())
+                + "' type='video/mp4'></video></body></html>";
+    }  // И в конце теста вызываем метод:  addVideo();
+
+    public static URL getVideoUrl(String sessionId) {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
+
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
+    // И в конце теста вызываем метод:  Attach.addVideo();
 }
 
